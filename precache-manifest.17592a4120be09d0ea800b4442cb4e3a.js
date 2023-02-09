f5a3bfaf18d43afd68fbcfa68ff4bcf5c7c8841d
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2023-02-09 17:01:10 -0300

    Updates

---
+++ precache-manifest.17592a4120be09d0ea800b4442cb4e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6370c9e324d210f430",
+    "url": "/pokemon-react/static/css/main.a51305a5.chunk.css"
+  },
+  {
+    "revision": "1a6370c9e324d210f430",
+    "url": "/pokemon-react/static/js/main.1a6370c9.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/pokemon-react/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "6f09fbbd169e2d5ddd0f",
+    "url": "/pokemon-react/static/js/2.6f09fbbd.chunk.js"
+  },
+  {
+    "revision": "7905a83fccc1f8b429b2",
+    "url": "/pokemon-react/static/js/runtime~main.7905a83f.js"
+  },
+  {
+    "revision": "f058f762c2373c74738f05d93e3a7adb",
+    "url": "/pokemon-react/index.html"
+  }
+];
